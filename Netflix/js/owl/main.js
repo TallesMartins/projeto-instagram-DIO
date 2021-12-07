d8815d087c77c30cf70d11324762366ece34ff4a
TallesMartins <tallessilvamartins@hotmail.com>
2021-12-07 20:46:22 -0300

    commit inicial

---
+++ Netflix/js/owl/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    center:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:7
+        }
+    }
+})
+function player(video){
+    let play = document.querySelector('.botao')
+    document
+}
